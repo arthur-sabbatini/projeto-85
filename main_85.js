019d54a5ca5cb11e487dac27253638eedd9df2ad
Arthur Araujo <arthur.sab.araujo@icloud.com>
2023-07-21 20:22:38 -0300

    initial commit

---
+++ main_85.js
@@ -0,0 +1,23 @@
+const menuListArray = [ "Pizza de Marguerita",                  
+                  "Pizza de Calabresa",
+                "Pizza de Quatro queijos","Pizza de Lombo","Pizza Portuguesa","Pizza de Atun","Pizza de banana com mel","Pizza Moda da casa"]; //Adicione mais nomes de pizza
+
+function getmenu()
+{
+    var htmldata="";
+    for(var i=0; i<menuListArray.length; i++)
+    {
+        htmldata=htmldata+ menuListArray[i] + '<br>'
+    }
+    //Dê um nome apropriado ao Id, como displayMenu 
+    document.getElementById("displayMenu").innerHTML = htmldata;
+}
+
+function add_suggestion(){
+    //Dê um nome de id apropriado, como addItem
+	var item = document.getElementById("addItem").value;
+    //Use a função push() para colocar o item no menuListArray
+    menuListArray.push(item);
+    console.log(menuListArray)
+}
+
